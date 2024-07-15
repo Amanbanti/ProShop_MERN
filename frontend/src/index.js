63fe99ae7f69907302d2476d6ce1a9c82eaf2c50
¨Amanbanti¨ <amanbanti2011@gmail.com>
2024-07-15 23:03:41 +0300

    productScreen Commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,12 +13,13 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
-
+import ProductScreen from './screens/ProductScreen';
 
 const router =createBrowserRouter( 
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
           <Route index={true} path="/" element={<HomeScreen/>}/>
+          <Route index={true} path="/product/:id" element={<ProductScreen/>}/>
        
     </Route>
   )
