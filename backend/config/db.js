a31417192c2afc98c118540b356c40679be2fcd1
Amanbanti <amanbanti2011@gmail.com>
2024-10-08 13:30:05 +0300

    server and db update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 60000, // 60 seconds
+    });
     console.log("MongoDB Connected: " + conn.connection.host);
   } catch (error) {
     console.error("Error connecting to MongoDB: ", error);
-    process.exit(1); 
+    process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,31 @@
 import express from 'express';
-import product from './data/product.js';
-import dotenv from 'dotenv'; 
-import {connectDB} from './config/db.js'
-
+import productRoute from './routes/productRoute.js';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-//port from .env file
-
-//connecting to mongoose db 
-connectDB();
-
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('API is Running...');
+  res.send('API is Running...');
 });
 
-app.get('/api/product', (req, res) => {
-    res.json(product);
-});
+app.use('/api/products', productRoute);
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/product/:id', (req, res) => {
-    const p = product.find((p) => p._id === req.params.id);
-    res.json(p);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log('Server running on port ' + port);
+    });
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+    process.exit(1); // Exit process with failure
+  }
+};
 
-app.listen(port, () => {
-    console.log('Server running on port ' + port);
-});
+startServer();
