098f9787add2fcbc295e92289883db5564c1f199
Amanbanti <amanbanti2011@gmail.com>
2024-10-08 13:30:34 +0300

    productRoute commit

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,29 @@
+import express from "express";
+import Product from '../models/productModel.js';
+import asyncHandler from "../middleware/asyncHandler.js";
+const router = express.Router();
+
+
+
+router.get('/',  asyncHandler (async(req, res) => {
+  
+    const products = await Product.find({}); // Fetch all users
+    res.json(products);
+      
+}));
+
+
+router.get('/:id',  asyncHandler ( async(req, res) => {
+    const product = await Product.findById(req.params.id);
+    if(product){
+        return  res.json(product);
+    }else{
+        res.status(404);
+        throw new Error('Resource not found!');
+    }
+   
+}));
+
+
+
+export default router;
