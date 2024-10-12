7cc05f614905ebe41f3face7b66a96df4915f75e
Amanbanti <amanbanti2011@gmail.com>
2024-10-12 19:28:59 +0300

    Cart Screen commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import {Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png'
-import  {LinkContainer} from 'react-router-bootstrap'
+import  {LinkContainer} from 'react-router-bootstrap';
+import { UseSelector, useSelector } from 'react-redux';
 const Header = () => {
+    const {cartItems} = useSelector((state)=> state.cart)
+  
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand='md' collapseOnSelect>
@@ -22,8 +25,14 @@ const Header = () => {
                     <Nav className='ms-auto'>
                         <LinkContainer to='/cart'>
                             <Nav.Link>
+                               
                                 <FaShoppingCart />
                                     Cart
+                                    {cartItems.length > 0 &&(
+                                        <Badge pill bg='success' style={{marginLeft:'5px'}}>
+                                            {cartItems.reduce((a,c)=>a + c.qty,0)}
+                                        </Badge>
+                                    )}
                                 </Nav.Link>
                         </LinkContainer>
                        
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,12 +16,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-
+import CartScreen from './screens/CartScreen';
 const router =createBrowserRouter( 
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
           <Route index={true} path="/" element={<HomeScreen/>}/>
           <Route index={true} path="/product/:id" element={<ProductScreen/>}/>
+          <Route index={true} path="/cart" element={<CartScreen/>}/>
        
     </Route>
   )
