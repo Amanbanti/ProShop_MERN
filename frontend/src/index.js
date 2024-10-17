e1f5e810b4b02cb602337f5fe1f869da80eaeaec
Amanbanti <amanbanti2011@gmail.com>
2024-10-18 00:56:02 +0300

    private route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -20,6 +20,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/shippingScreen';
+import PrivateRoute from './components/PrivateRoute';
 const router =createBrowserRouter( 
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
@@ -28,7 +29,11 @@ const router =createBrowserRouter(
           <Route  path="/cart" element={<CartScreen/>}/>
           <Route  path="/login" element={<LoginScreen/>}/>
           <Route  path="/register" element={<RegisterScreen/>}/>
-          <Route  path="/shipping" element={<ShippingScreen/>}/>
+         
+
+          <Route path="" element={<PrivateRoute/>}>
+              <Route  path="/shipping" element={<ShippingScreen/>}/>
+          </Route>
     </Route>
   )
 )
