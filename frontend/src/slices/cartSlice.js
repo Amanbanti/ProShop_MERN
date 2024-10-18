b1ca53306ed7cdcfa97ec27ecc1a48428d6ad345
Amanbanti <amanbanti2011@gmail.com>
2024-10-18 16:18:27 +0300

    orderapislice commit

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -59,13 +59,25 @@ const cartSlice = createSlice({
         savePaymentMethod : (state , action) =>{
             state.paymentMethod = action.payload
         
-
             return updateCart(state);
-
+            
+        },
+        clearCartItems : (state , action) =>{
+            state.cartItems = []
+        
+            return updateCart(state);
             
         }
     },
 })
 
-export const {addToCart , removeFromCart,saveShippingAddress,savePaymentMethod} = cartSlice.actions;
+export const {
+    addToCart ,
+     removeFromCart,
+     saveShippingAddress,
+     savePaymentMethod,
+     clearCartItems,
+    
+} = cartSlice.actions;
+
 export default cartSlice.reducer;
---
+++ frontend/src/slices/ordersApiSlice.js
@@ -0,0 +1,21 @@
+import { ORDERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const ordersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        createOrder: builder.mutation({
+            query: (order) => ({
+                url:  ORDERS_URL, 
+                method :'POST',
+                body:{...order},
+            }),
+        
+        }),
+
+   
+
+ 
+    }),
+});
+
+export const { useCreateOrderMutation } = ordersApiSlice;
